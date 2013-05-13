377a81f27911fa03aebf12c5dfd86480bc16d6d5
Dustin Morrill <dmorrill10@gmail.com>
2013-05-13 11:39:48 -0600

    Minor changes to spec_helper.

--- spec/support/spec_helper.rb
+++ spec/support/spec_helper.rb
@@ -2,30 +2,32 @@ require 'simplecov'
 SimpleCov.start
 
 require 'minitest/spec'
-require 'minitest/pride'
 require 'minitest/mock'
 
-require 'turn'
-Turn.config do |c|
-  # use one of output formats:
-  # :outline  - turn's original case/test outline mode [default]
-  # :progress - indicates progress with progress bar
-  # :dotted   - test/unit's traditional dot-progress mode
-  # :pretty   - new pretty reporter
-  # :marshal  - dump output as YAML (normal run mode only)
-  # :cue      - interactive testing
-  c.format  = :dotted
-  # use humanized test names (works only with :outline format)
-  c.natural = true
-end
-
-require 'awesome_print'
-module Minitest::Assertions
-  def mu_pp(obj)
-    obj.awesome_inspect
+begin
+  require 'turn/autorun'
+  Turn.config do |c|
+    # use one of output formats:
+    # :outline  - turn's original case/test outline mode [default]
+    # :progress - indicates progress with progress bar
+    # :dotted   - test/unit's traditional dot-progress mode
+    # :pretty   - new pretty reporter
+    # :marshal  - dump output as YAML (normal run mode only)
+    # :cue      - interactive testing
+    c.format  = :dotted
+    # use humanized test names (works only with :outline format)
+    c.natural = true
   end
-end
 
-require 'pry-rescue/minitest'
+  require 'awesome_print'
+  module Minitest::Assertions
+    def mu_pp(obj)
+      obj.awesome_inspect
+    end
+  end
+
+  require 'pry-rescue/minitest'
+rescue LoadError
+end
 
 require 'ruby-duration'
