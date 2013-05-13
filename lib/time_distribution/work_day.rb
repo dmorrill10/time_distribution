ab840e9312d671cbd7625e9ab0a1230237c6aca9
Dustin Morrill <dmorrill10@gmail.com>
2013-05-13 11:37:12 -0600

    Added functionality to WorkDay.time_worked.

--- lib/time_distribution/work_day.rb
+++ lib/time_distribution/work_day.rb
@@ -19,12 +19,26 @@ module TimeDistribution
       self
     end
 
-    def time_worked
+    def time_worked(*subjects)
+      subjects.flatten!
       times = {}
-      @tasks.inject({}) do |times, t|
-        times[t.subject] = Duration.new(0) unless times[t.subject]
-        times[t.subject] += t.time_taken
+      unless subjects.empty? || subjects.kind_of?(Set)
+        subjects = Set.new(subjects) # To take advantage of Set's faster include? check
+      end
+
+      times_to_return = @tasks.inject({}) do |times, t|
+        t_subject = t.subject
+        next times unless subjects.empty? || subjects.include?(t_subject)
+
+        times[t_subject] = Duration.new(0) unless times[t_subject]
+        times[t_subject] += t.time_taken
         times
+      end
+
+      if times_to_return.length == 1
+        times_to_return.values.first
+      else
+        times_to_return
       end
     end
   end
--- spec/work_day_spec.rb
+++ spec/work_day_spec.rb
@@ -22,16 +22,94 @@ describe WorkDay do
     end
   end
   describe 'time_worked' do
-    it 'works' do
+    describe 'returns a hash of duration sums without arguments' do
+      it 'when all tasks have the same subject' do
+        time_taken_for_each_task = 23
+        x_subject = 'my subject'
+        @x_tasks.each do |mock_task|
+          mock_task.expect(:subject, x_subject)
+          mock_task.expect(:time_taken, time_taken_for_each_task)
+        end
+        @patient.time_worked.must_equal (Duration.new(@x_tasks.length * time_taken_for_each_task))
+      end
+      it 'when more than one subject is present' do
+        time_taken_for_each_task = 23
+        subject_1 = 'subject 1'
+        @x_tasks.each do |mock_task|
+          mock_task.expect(:subject, subject_1)
+          mock_task.expect(:time_taken, time_taken_for_each_task)
+        end
+        num_subject_1_tasks = @x_tasks.length
+        t3 = MiniTest::Mock.new
+        subject_2 = 'subject 2'
+        t3.expect(:subject, subject_2)
+        t3.expect(:time_taken, time_taken_for_each_task)
+        @x_tasks << t3
+        t4 = MiniTest::Mock.new
+        subject_3 = 'subject 3'
+        t4.expect(:subject, subject_3)
+        t4.expect(:time_taken, time_taken_for_each_task)
+        @x_tasks << t4
+
+        @patient.time_worked.must_equal (
+          {
+            subject_1 => Duration.new(num_subject_1_tasks * time_taken_for_each_task),
+            subject_2 => Duration.new(time_taken_for_each_task),
+            subject_3 => Duration.new(time_taken_for_each_task)
+          }
+        )
+      end
+    end
+    it 'returns a hash of duration sums when given a list of subjects' do
       time_taken_for_each_task = 23
-      x_subject = 'my subject'
+      subject_1 = 'subject 1'
       @x_tasks.each do |mock_task|
-        mock_task.expect(:subject, x_subject)
-        mock_task.expect(:subject, x_subject)
-        mock_task.expect(:subject, x_subject)
+        mock_task.expect(:subject, subject_1)
         mock_task.expect(:time_taken, time_taken_for_each_task)
       end
-      @patient.time_worked.must_equal ({x_subject => Duration.new(@x_tasks.length * time_taken_for_each_task)})
+      num_subject_1_tasks = @x_tasks.length
+      t3 = MiniTest::Mock.new
+      subject_2 = 'subject 2'
+      t3.expect(:subject, subject_2)
+      t3.expect(:time_taken, time_taken_for_each_task)
+      @x_tasks << t3
+      t4 = MiniTest::Mock.new
+      subject_3 = 'subject 3'
+      t4.expect(:subject, subject_3)
+      @x_tasks << t4
+
+      @patient.time_worked([subject_1, subject_2]).must_equal (
+        {
+          subject_1 => Duration.new(num_subject_1_tasks * time_taken_for_each_task),
+          subject_2 => Duration.new(time_taken_for_each_task)
+        }
+      )
+    end
+    describe 'returns a single sum given a subject' do
+      it 'as a scalar' do
+        time_taken_for_each_task = 23
+        subject_1 = 'subject 1'
+        @x_tasks.each do |mock_task|
+          mock_task.expect(:subject, subject_1)
+          mock_task.expect(:time_taken, time_taken_for_each_task)
+        end
+        num_subject_1_tasks = @x_tasks.length
+        t3 = MiniTest::Mock.new
+        subject_2 = 'subject 2'
+        t3.expect(:subject, subject_2)
+        t3.expect(:time_taken, time_taken_for_each_task)
+        @x_tasks << t3
+        t4 = MiniTest::Mock.new
+        subject_3 = 'subject 3'
+        t4.expect(:subject, subject_3)
+        @x_tasks << t4
+
+        @patient.time_worked(subject_1).must_equal (
+          Duration.new(num_subject_1_tasks * time_taken_for_each_task)
+        )
+      end
+      it 'as a single element list' do
+      end
     end
   end
 end
