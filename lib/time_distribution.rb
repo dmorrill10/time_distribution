3fd74d0121690d734fb217c0f65b00d88419eccd
Dustin Morrill <dmorrill10@gmail.com>
2015-03-14 21:12:16 -0600

    Adds the two new classes from last commit to the library file

--- lib/time_distribution.rb
+++ lib/time_distribution.rb
@@ -4,7 +4,9 @@ require 'time_distribution/time'
 
 require 'time_distribution/smart_duration'
 require 'time_distribution/task'
+require 'time_distribution/task_list'
 require 'time_distribution/work_day'
+require 'time_distribution/work_day_collection'
 
 module TimeDistribution
 end
